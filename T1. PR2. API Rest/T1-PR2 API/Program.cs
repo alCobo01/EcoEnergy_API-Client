96a89b674b4ebd2e5016225131306f66564a7200
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-04-03 11:58:15 +0200

    New branch

--- T1. PR2. API Rest/T1-PR2 API/Program.cs
+++ T1. PR2. API Rest/T1-PR2 API/Program.cs
@@ -7,6 +7,7 @@ namespace T1_PR2_API
     {
         public static void Main(string[] args)
         {
+            //
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
