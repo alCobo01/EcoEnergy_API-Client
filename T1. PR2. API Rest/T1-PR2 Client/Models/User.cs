f84499d5f7228fa6abefe19c9399ac0e88000565
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-04-10 13:55:06 +0200

    Treball a classe 10/04

---
+++ T1. PR2. API Rest/T1-PR2 Client/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace T1_PR2_Client.Models
+{
+    public class User
+    {
+        [Required]
+        [DisplayName("Name and surnames")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The password must have minimum 8 characters", MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and the confirmation password must be the same!")]
+        public string ConfirmationPassword { get; set; }
+    }
+}
---
+++ T1. PR2. API Rest/T1-PR2 Client/Pages/Register.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using T1_PR2_Client.Models;
+using T1_PR2_Client.Models.DTOs;
+
+namespace T1_PR2_Client.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        [BindProperty]
+        public User User { get; set; }
+
+        public string ApiErrorMessage { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Map User to RegisterUserDTO to avoid sending ConfirmedPassword
+            var registerUserDto = new RegisterUserDTO
+            {
+                Name = User.Name,
+                Email = User.Email,
+                UserName = User.UserName,
+                Password = User.Password
+            };
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7003/api/auth/");
+                var response = await client.PostAsJsonAsync("register", registerUserDto);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToPage("/Index");
+                }
+                else
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    ApiErrorMessage = $"Error en el registro ({response.StatusCode}): {responseBody}";
+                   
+                    ModelState.AddModelError(string.Empty, "Falló el registro en la API. Inténtalo de nuevo o contacta soporte."); // Mensaje genérico al usuario
+                    return Page();
+                }
+            }
+        }
+    }
+}
