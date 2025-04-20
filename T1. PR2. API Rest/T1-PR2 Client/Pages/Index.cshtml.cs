bbda079f17feffaaaffdd8f8728d3e955090f966
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-04-20 13:11:30 +0200

    List of games implemented

--- T1. PR2. API Rest/T1-PR2 Client/Pages/Index.cshtml.cs
+++ T1. PR2. API Rest/T1-PR2 Client/Pages/Index.cshtml.cs
@@ -1,19 +1,51 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using T1_PR2_Client.Models;
 
 namespace T1_PR2_Client.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    private readonly IConfiguration _configuration;
+    public IndexModel(IConfiguration configuration)
     {
-        _logger = logger;
+        _configuration = configuration;
     }
 
-    public void OnGet()
-    {
+    public List<Game> Games { get; set; } = new();
 
+    public string ApiErrorMessage { get; set; } = string.Empty;
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var baseUrl = _configuration["ApiBaseUrl"];
+        try
+        {
+            using var client = new HttpClient() { };
+            client.BaseAddress = new Uri(baseUrl);
+
+            var response = await client.GetAsync("api/games");
+            if (response.IsSuccessStatusCode)
+            {
+                var gamesFromApi = await response.Content.ReadFromJsonAsync<List<Game>>();
+                if (gamesFromApi != null)
+                    Games = gamesFromApi
+                        .OrderByDescending(g => g.RatedUsers.Count)
+                        .ToList();
+            }
+            else
+            {
+                var body = await response.Content.ReadAsStringAsync();
+                ApiErrorMessage = $"Error retrieving games: ({response.StatusCode}): {body}";
+                ModelState.AddModelError(string.Empty, ApiErrorMessage);
+            }
+        }
+        catch (Exception ex)
+        {
+            ApiErrorMessage = $"Exception calling API: {ex.Message}";
+            ModelState.AddModelError(string.Empty, ApiErrorMessage);
+        }
+
+        return Page();
     }
 }
