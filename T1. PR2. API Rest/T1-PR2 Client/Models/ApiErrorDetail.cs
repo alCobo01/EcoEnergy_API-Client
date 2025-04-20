cc76658f55417995dadfb4d36e969620f08eaf17
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-04-20 17:10:34 +0200

    Detail started

--- T1. PR2. API Rest/T1-PR2 Client/Models/ApiErrorDetail.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace T1_PR2_Client.Models
-{
-    public class ApiErrorDetail
-    {
-        public string Code { get; set; }
-        public string Description { get; set; }
-    }
-
-}
---
+++ T1. PR2. API Rest/T1-PR2 Client/Pages/GameDetail.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using T1_PR2_Client.Models;
+using T1_PR2_Client.Services;
+
+namespace T1_PR2_Client.Pages
+{
+    public class GameDetailModel : PageModel
+    {
+        public readonly GameService _gameService;
+
+        public GameDetailModel(GameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        public Game Game { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Game = await _gameService.GetGameByIdAsync(id);
+            return Page();
+        }
+    }
+}
